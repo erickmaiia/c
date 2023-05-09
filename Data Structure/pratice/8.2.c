980610f97fe2f8784b892322a35f622aff6e4086
Erick Maia <agnaldo.oliveira2004@gmail.com>
2023-05-09 19:41:31 -0300

    update

--- Data Structure/pratice/8.2.c
+++ Data Structure/pratice/8.2.c
@@ -6,48 +6,50 @@ typedef struct node
     int value;
     struct node *next;
 } NODE;
+typedef struct queue
+{
+    NODE *start;
+    NODE *end;
+    int size;
+} QUEUE;
 
-NODE *start = NULL;
-NODE *end = NULL;
-int size = 0;
-
-void add(int value)
+void appendQueue(int value, QUEUE *q)
 {
 
     NODE *new = malloc(sizeof(NODE));
     new->value = value;
     new->next = NULL;
 
-    if (start == NULL)
+    if (q->start == NULL)
     {
-        start = new;
-        end = new;
+        q->start = new;
+        q->end = new;
     }
     else
     {
-        end->next = new;
-        end = new;
+        q->end->next = new;
+        q->end = new;
     }
-    size++;
+    q->size++;
 }
 
-void imprimir()
+void printQueue(QUEUE *q)
 {
 
-    NODE *hook = start;
+    NODE *hook = q->start;
 
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < q->size; i++)
     {
         printf("Value: %d\n", hook->value);
         hook = hook->next;
     }
 }
 
-int remover()
+int popQueue(QUEUE *q)
 {
 
-    NODE *hook = start->next;
-    NODE *bigger = start;
+    NODE *hook = q->start->next;
+    NODE *bigger = q->start;
     int value;
     while (hook->next != NULL)
     {
@@ -57,36 +59,50 @@ int remover()
         }
         hook = hook->next;
     }
-    if (bigger->next->value > start->value)
+    if (bigger->next->value > q->start->value)
     {
 
         NODE *garbage = bigger->next;
         value = garbage->value;
-        if (bigger->next->value == end->value)
+        if (bigger->next->value == q->end->value)
         {
-            end = bigger;
+            q->end = bigger;
         }
         bigger->next = bigger->next->next;
         free(garbage);
     }
     else
     {
-        NODE *garbage = start;
-        value = start->value;
-        start = start->next;
+        NODE *garbage = q->start;
+        value = q->start->value;
+        q->start = q->start->next;
         free(garbage);
     }
-    size--;
+    q->size--;
     return value;
+}
+
+QUEUE *startQueue(void)
+{
+    QUEUE *hook = malloc(sizeof(QUEUE));
+    hook->end = NULL;
+    hook->start = NULL;
+    hook->size = 0;
+    return hook;
 }
 
 int main()
 {
-    add(0);
-    add(1);
-    add(1);
-    add(2);
-    add(3);
-    printf("Removed value: %d\n", remover());
+    QUEUE *q1 = startQueue();
+
+    appendQueue(0, q1);
+    appendQueue(1, q1);
+    appendQueue(1, q1);
+    appendQueue(2, q1);
+    appendQueue(3, q1);
+
+    printQueue(q1);
+    printf("Removed value: %d\n", popQueue(q1));
+    printQueue(q1);
     return 0;
 }
